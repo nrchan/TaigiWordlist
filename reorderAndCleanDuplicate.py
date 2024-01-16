dcbc9fb71f59faffce726b19ea8ed8bc9f861994
nrchan <luka.iwt@gmail.com>
2024-01-16 11:05:58 +0800

    reorder

--- reorderAndCleanDuplicate.py
+++ reorderAndCleanDuplicate.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     df = pd.read_csv('TaigiDatabase.csv')
 
     df['Hanji'] = pd.Categorical(df.Hanji, categories=df["Hanji"].unique().tolist(), ordered=True)
-    df['TL'] = pd.Categorical(df.TL, categories=df["TL"].unique().tolist(), ordered=True)
+    df['TL'] = pd.Categorical(df.TL, ordered=True)
     df.sort_values(["Hanji","TL"], inplace=True)
 
     df.drop_duplicates(inplace=True)
